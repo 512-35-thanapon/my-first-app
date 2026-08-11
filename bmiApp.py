f23f45351d2c144b3c02db421969c5c531e1c6f5
512-35-thanapon <56716@yupparaj.ac.th>
2026-08-11 10:51:53 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -4,7 +4,7 @@ st.markdown("# :red[คำนวณค่าดัชนีมวลกาย BM
 st.write("กรอกข้อมูลนํ้าหนักและส่วนสูงขอกคุณ เพื่อเช็กสุขภาพเบี้องต้น")
 
 weight = st.number_input("กรอกนํ้าหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
-height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
+height.cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
 
 if st.button("คำนวณค่า BMI"):
   height.m = height.cm / 100
