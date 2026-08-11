ef512951d23f58f4fcdc313abac4924e9d7c8005
512-35-thanapon <56716@yupparaj.ac.th>
2026-08-11 10:43:49 +0700

    Create bmiApp.py

---
+++ bmiApp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.markdown("# :red[คำนวณค่าดัชนีมวลกาย BMI]")\
+st.write("กรอกข้อมูลนํ้าหนักและส่วนสูงขอกคุณ เพื่อเช็กสุขภาพเบี้องต้น")
+
+weight = st.number_input("กรอกนํ้าหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
+height = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
+
+if st.button("คำนวณค่า BMI"):
+  height.m = height.cm / 100
+  bmi = weight / (height_m ** 2)
+
+st.write("---")
+st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+
+if bmi < 18.5:\
+  st.warning("คุณมีนํ้าหนักน้อยกว่าเกณฑ์ (ผอม)")
+elif 18.5 <= bmi < 23.0:
+  st.success("คุณมีนํ้าหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+elif 23.0 <= bmi < 25.0:
+  st.info("คุณเริ่มมีนํ้าหนักเกินเกณฑ์ (ท้วม)")
+else:
+  st.error("คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
+
+st.divider
+st.write("นายธนพล ยะวัน เลขที่ 35 ม.4/12")
